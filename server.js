8a2cb91d06cc03aae22c694d02a737ffb2c932e8
David Marlowe <webpropopuli@gmail.com>
2019-06-25 11:03:51 -0400

    working on adding the post time - it's in a whole different tree

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const express = require('express')
+const express = require("express");
+const app = express();
 
-const scraper = require('./utils/scraper')
-const app = express()
+const scraper = require("./utils/scraper");
+const { scrapeMedium, scrapeYoutube } = require("./utils/scraper");
 
-app.set('view engine', 'pug')
+app.set("view engine", "pug"); // could be anything
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const mediumArticles = new Promise((resolve, reject) => {
-    scraper
-      .scrapeMedium()
+    scrapeMedium()
       .then(data => {
-        resolve(data)
+        resolve(data);
       })
-      .catch(err => reject('Medium scrape failed'))
-  })
+      .catch(err => reject("Medium scrape failed"));
+  });
 
   const youtubeVideos = new Promise((resolve, reject) => {
-    scraper
-      .scrapeYoutube()
+    scrapeYoutube()
       .then(data => {
-        resolve(data)
+        resolve(data);
       })
-      .catch(err => reject('YouTube scrape failed'))
-  })
+      .catch(err => reject("YouTube scrape failed"));
+  });
 
   Promise.all([mediumArticles, youtubeVideos])
     .then(data => {
-      res.render('index', { data: { articles: data[0], videos: data[1] } })
+      res.render("index", { data: { articles: data[0], videos: data[1] } });
     })
-    .catch(err => res.status(500).send(err))
-})
+    .catch(err => res.status(500).send(err));
+});
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
--- utils/scraper.js
+++ utils/scraper.js
@@ -1,47 +1,43 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
 
+//####
 const scrapeMedium = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://medium.com/search?q=headless%20browser')
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://medium.com/search?q=headless%20browser");
 
   const scrapedData = await page.evaluate(() =>
-    Array.from(
-      document.querySelectorAll(
-        'div.postArticle-content a:first-child[data-action-value]'
-      )
-    )
-      .filter(node => node.querySelector('.graf--title'))
+    Array.from(document.querySelectorAll("div.postArticle-content a:first-child[data-action-value]"))
+      //.filter(node => node.querySelector(".graf--title"))
       .map(link => ({
-        title: link.querySelector('.graf--title').textContent,
-        link: link.getAttribute('data-action-value')
+        title: link.querySelector(".graf--title").textContent,
+        link: link.getAttribute("data-action-value"),
+        rest: link.querySelector("body").innerText)
       }))
-  )
+  );
 
-  await browser.close()
-  return scrapedData
-}
+  await browser.close();
+  return scrapedData;
+};
 
+//########
 const scrapeYoutube = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(
-    'https://www.youtube.com/results?search_query=headless+browser'
-  )
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.youtube.com/results?search_query=headless+browser");
 
   const scrapedData = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('.ytd-video-renderer #video-title'))
+    Array.from(document.querySelectorAll(".ytd-video-renderer #video-title"))
       .map(link => ({
-        title: link.getAttribute('title'),
-        link: link.getAttribute('href')
+        title: link.getAttribute("title"),
+        link: link.getAttribute("href")
       }))
       .slice(0, 10)
-  )
+  );
 
+  await browser.close();
+  return scrapedData;
+};
 
-  await browser.close()
-  return scrapedData
-}
-
-module.exports.scrapeMedium = scrapeMedium
-module.exports.scrapeYoutube = scrapeYoutube
+module.exports.scrapeMedium = scrapeMedium;
+module.exports.scrapeYoutube = scrapeYoutube;
